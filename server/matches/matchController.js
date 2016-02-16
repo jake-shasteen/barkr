0dbca7140d063568ac6240ed66bb5b0f08141580
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-16 15:46:18 -0800

    Build query for unmatched dogs by user id

--- server/matches/matchController.js
+++ server/matches/matchController.js
@@ -1,10 +1,11 @@
 var helpers = require( '../config/helpers' );
+var db = require( '../config/db' );
 var Match = require( './match' );
 var Dog = require( '../dogs/dog' );
 
 module.exports = {
   showMatches: function( req, res, next ) {
-    var userid = req.params.userid;
+    var userid = parseInt( req.params.userid );
     Match.findAll( { where: {'u_id': userid, 'vote': true } } ).then( function( matches ) {
       res.json( matches );
     },
@@ -14,13 +15,19 @@ module.exports = {
   },
 
   showUnmatchedDogIds: function( req, res, next ) {
-    var userid = req.params.userid;
-    Match.findAll( { attributes: [], where: {'u_id': userid }, include: { model: Dog,  } } )
+    var userid = parseInt( req.params.userid );
+    db.query("SELECT dogs.id FROM dogs WHERE dogs.id NOT IN ( SELECT d_id FROM matches WHERE u_id = " + userid + ")", { type: db.QueryTypes.SELECT })
+    .then( function( dog_ids ) {
+      res.json( dog_ids );
+    },
+    function( err ) {
+      helpers.reportError( res, err );
+    });
   },
 
   upvote: function( req, res, next ) {
-    var userid = req.params.userid;
-    var dogid = req.params.dogid;
+    var userid = parseInt( req.params.userid );
+    var dogid = parseInt( req.params.dogid );
     Match.create( {'u_id': userid, 'd_id': dogid, 'vote': true } )
     .then( function( match ) {
       res.send( "Matcher: " + userid + " matched: " + dogid )
@@ -31,8 +38,8 @@ module.exports = {
   },
 
   downvote: function( req, res, next ) {
-    var userid = req.params.userid;
-    var dogid = req.params.dogid;
+    var userid = parseInt( req.params.userid );
+    var dogid = parseInt( req.params.dogid );
     Match.create( {'u_id': userid, 'd_id': dogid, 'vote': false } )
     .then( function( match ) {
       res.send( "Matcher: " + userid + " declined: " + dogid )
