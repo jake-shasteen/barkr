febd90b8445ac9469ffd69f8cf1ad0058f8f3afe
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-15 19:02:49 -0800

    Add API endpoint for showing one dog profile

--- server/dogs/dogController.js
+++ server/dogs/dogController.js
@@ -12,6 +12,16 @@ module.exports = {
     });
   },
 
+  showOne: function( req, res, next ) {
+    var id = req.params.id;
+    Dog.find({ 'id': id })
+    .then( function( dog ) {
+      res.json( dog );
+    }, function( err ) {
+      helpers.reportError( res, err );
+    })
+  },
+
   addOne: function( req, res, next ) {
     Dog.create( req.body ).then( function() {
       res.send( "New dog profile added!" );
