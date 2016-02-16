ac28c193ff2179e210ed853f2d122af3668b0aec
Jake Shasteen <jakeshasteen@gmail.com>
2016-02-15 22:24:42 -0800

    Display random dog

--- client/app/find/find.js
+++ client/app/find/find.js
@@ -2,9 +2,18 @@ angular.module( 'barkr.find', [] )
 
 .controller( 'FindController', function( $scope, Find ) {
   $scope.dog = {};
-  $scope.dog.images = ['http://i.imgur.com/AwdYxQN.png'];
-  $scope.dog.primaryImage = $scope.dog.images[0];
-  $scope.dog.name = "Buddy";
-  $scope.dog.distance = ".5 mi";
-  $scope.dog.profile = "Buddy is a Good Boy. He likes tennis balls and running in circles. He dislikes cats and the color yellow. He knows his own name because he is a good boy.";
+  $scope.dog.images = [];
+  $scope.dog.primaryImage = '';
+
+  Find.randomDog( function( dog ) {
+    $scope.dog = dog;
+
+    Find.fillImages( dog.id, function( images ) {
+      images = _.pluck( images, 'path' );
+      $scope.dog.images = images;
+      $scope.dog.primaryImage = images[0];
+    });
+
+  });
+
 } );
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,11 +1,57 @@
 angular.module('barkr.services', [])
 
 .factory( 'Find', function( $http ) {
-  return $http({
+
+  var allDogs = function( callback ) {
+    return $http({
+        method: 'GET',
+        url: '/api/dogs'
+      })
+      .then( function ( resp ) {
+        callback( resp.data );
+      });
+  };
+
+  var countDogs = function( callback ) {
+    return $http({
       method: 'GET',
-      url: '/api/dogs'
+      url: '/api/dogs/count'
     })
-    .then( function ( resp ) {
-      return resp.data;
+    .then( function( resp ) {
+      callback( resp.data );
     });
+  }
+
+  var randomDog = function( callback ) {
+    return $http({
+      method: 'GET',
+      url: '/api/dogs/count'
+    })
+    .then( function( resp ) {
+      var pickDog = Math.floor( Math.random() * resp.data )+1;
+      return $http({
+        method: 'GET',
+        url: 'api/dogs/' + pickDog
+      })
+      .then( function( resp ) {
+        callback( resp.data );
+      })
+    });
+  }
+
+  var fillImages = function( id, callback ) {
+    return $http({
+      method: 'GET',
+      url: '/api/images/dog/' + id
+    }).then( function( resp ) {
+      callback( resp.data );
+    })
+  }
+
+  return {
+    allDogs: allDogs,
+    countDogs: countDogs,
+    randomDog: randomDog,
+    fillImages: fillImages
+  }
 } );
